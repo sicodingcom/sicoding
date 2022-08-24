598c826e0d7b0c7cec78ecfd783733913d06a386
SiCoding <sicoding.com@gmail.com>
2022-08-24 08:11:04 +0700

    Sicoding Initialize

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="id">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  @stack('meta')
+
+  @livewireStyles
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-PYTK9LWZDZ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-PYTK9LWZDZ');
+  </script>
+
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != 'dataLayer' ? '&l=' + l : '';
+      j.async = true;
+      j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-WX5V7FR');
+  </script>
+  <!-- End Google Tag Manager -->
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WX5V7FR" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <div class="container">
+    {{ $slot }}
+  </div>
+
+  @livewireScripts
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ resources/views/pages/homes/index.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+  <div class="home-section">
+    <h1 class="h1 text-center">SiCoding - Judul home</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero impedit possimus eaque quas tempora omnis
+      incidunt fugit ipsum tempore odio non nulla laboriosam, rem veniam illo sit similique, dolorem qui?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ratione possimus velit distinctio qui delectus
+      quibusdam quam ipsam voluptatum animi nisi mollitia cum nulla temporibus dolore, perferendis, atque perspiciatis
+      natus?
+    </p>
+  </div>
+</x-app-layout>
+
+@push('meta')
+  <title>SiCoding - Judul</title>
+  <meta name="description"
+    content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero impedit possimus eaque quas tempora omnis incidunt fugit ipsum tempore odio non nulla laboriosam, rem veniam illo sit similique, dolorem qui?">
+  <meta name="keywords" content="Lorem ipsum, dolor sit, amet consectetur, adipisicing elit, Libero impedit, possimus">
+
+  <meta name="googlebot-news" content="index, follow" />
+  <meta name="googlebot" content="index, follow" />
+
+  <meta name="robots" content="index, follow" />
+  <meta name="robots" content="max-image-preview:large">
+
+  <meta name="author" content="SiCoding">
+  <meta name="language" content="id" />
+  <meta name="geo.country" content="id" />
+  <meta http-equiv="content-language" content="In-Id" />
+  <meta name="geo.placename" content="Indonesia" />
+@endpush
